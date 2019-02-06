bc2164165474478c19c5d288e4c448d364893173
William <lbkhanhvu@gmail.com>
2019-02-06 19:49:30 +0900

    add 'count' page

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage),
+    path('count/', views.count, name='count'),
 ]
--- wordcount/views.py
+++ wordcount/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'home.html')
+
+def count(request):
+    return render(request, 'count.html')
